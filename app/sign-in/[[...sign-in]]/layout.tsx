fac8d20f15c3ef6a1dca9b98cc4d7fca973b2afc
subrajeet-maharana <subrajeet.info@gmail.com>
2023-11-13 19:39:18 +0530

    homepage with clerk auth integration

---
+++ app/sign-in/[[...sign-in]]/layout.tsx
@@ -0,0 +1,11 @@
+export default function Layout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            {children}
+        </div>
+    )
+}
